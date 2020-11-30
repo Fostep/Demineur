48ae9dce1daba171595d813589a81bd695f665e5
Fostep <73890826+Fostep@users.noreply.github.com>
2020-11-30 20:55:34 +0100

    Update demineur.py

--- demineur.py
+++ demineur.py
@@ -1,114 +1,178 @@
-from random import randint
+import random
 
-# -------------------------------- FONCTIONS --------------------------------
-#Ajoute aléatoirement les bombes dans la matrice
-#Renvoie la matrice
-def AleaBombe(list):
-  incr = 0
-  while(incr<20):
-    ligne = randint(0,8)
-    colonne = randint(0,8)
+# ------------------- FONCTIONS  -------------------
 
-    if(list[ligne][colonne] != 'b'):
-      list[ligne][colonne] = 'b'
-      incr += 1
-      list = ajoutCaseAdj(list, ligne, colonne)
-  return list
+# Pose des bombes aleatoirement dans la matrice choisie
+# pas de return, pas de print
+def bombeAleatoire(matrice, chiffre):
+  nbrBombe = 0
+  while(nbrBombe < (chiffre*chiffre)//3):
 
-#Vérifie que l'on peut augmenter de +1 les cases autour de la mise en place de la bombe
-#Renvoie la matrice
-def ajoutCaseAdj(liste, ligB, colB):
-  if (ligB-1>=0 and colB-1>=0):
-    if(liste[ligB-1][colB-1] != 'b'):
-      liste[ligB-1][colB-1] = liste[ligB-1][colB-1]+1
+    bombeLigne = random.randint(0,len(matrice)-1)
+    bombeColonne = random.randint(0,len(matrice[0])-1)
+    if(matrice[bombeLigne][bombeColonne] != 'b'):
+      matrice[bombeLigne][bombeColonne] = 'b'
+      chiffreCase(matrice, bombeLigne, bombeColonne)
+      nbrBombe += 1 #nbrBombe = nbrBombe + 1
 
-  if(ligB-1>=0):
-    if(liste[ligB-1][colB] != 'b'):
-      liste[ligB-1][colB] = liste[ligB-1][colB]+1
+# Place +1 aux cases adjacentes d'une bombe
+# pas de return, pas de print
+def chiffreCase(matrice, ligneBombe, colonneBombe):
+  #Regarde les 8 cases autour
+  for i in range(ligneBombe-1, ligneBombe+2):
+    for y in range(colonneBombe-1, colonneBombe+2):
+      #Regarde si bien dans la matrice
+      if i in range(0,len(matrice)):
+        if y in range(0, len(matrice[0])):
+          if matrice[i][y] != 'b':
+            matrice[i][y] +=1
 
-  if(ligB-1>=0 and colB+1<9):
-    if(liste[ligB-1][colB+1] != 'b'):
-      liste[ligB-1][colB+1] = liste[ligB-1][colB+1]+1
-
-  if(colB-1>=0):
-    if(liste[ligB][colB-1] != 'b'):
-      liste[ligB][colB-1] = liste[ligB][colB-1]+1
-
-  if(colB+1<0):
-    if(liste[ligB][colB+1] != 'b'):
-      liste[ligB][colB+1] = liste[ligB][colB+1]+1
-
-  if(ligB+1<9 and colB-1>=0):
-    if(liste[ligB+1][colB-1] != 'b'):
-      liste[ligB+1][colB-1] = liste[ligB+1][colB-1]+1
-
-  if(ligB+1<9):
-    if(liste[ligB+1][colB] != 'b'):
-      liste[ligB+1][colB] = liste[ligB+1][colB]+1
-
-  if(ligB+1<9 and colB+1<9):
-    if(liste[ligB+1][colB+1] != 'b'):
-      liste[ligB+1][colB+1] = liste[ligB+1][colB+1]+1
-  
-  return liste
-
-#Affiche la liste servant à afficher
-#Renvoie rien
-def AfficherMatrice(list):
-  print("   1 2 3 4 5 6 7 8 9")
-  print("  ------------------")
-  for i in range(0,9):
-    print(i+1, end="")
-    print("|", end="")
-    for y in range(0,9):
-      print(" ", end="")
-      print(list[i][y], end="")
+# Affiche la matrice choisie
+# pas de return, print(matrice)
+def Afficher(matrice, chiffre):
+  #Affiche les numéros des colonnes
+  print("   ",end="")
+  for i in range(0, chiffre):
+    print(i+1, end=" ")
+  print("")
+  #Affiche la ligne dessous
+  print("  ",end="")
+  print(chiffre*"--")
+  #Affiche la matrice et le numéro des lignes
+  for ligne in range(0,len(matrice)):
+    print(ligne+1, end="| ")
+    for colone in range(0,len(matrice[0])):
+      print(matrice[ligne][colone], end=" ")
     print("")
 
-#Choix de la ligne et colonne, boucle si incorrect.
-#Renvoie ligne et colonne
-def ChoixJoueur():
-  print("Choix ligne: ", end="")
-  ligne= int(input())
+# Creer les 2 matrices comportant les bombes et l'affichage
+# return les 2 matrices, pas de print
+def creerMatrice(chiffre):
+  listeDem = []
+  listeAff = []
+  #Demineur
+  for ligneDem in range(0, chiffre):
+    listeDem.append([])
+    for colonneDem in range(0,chiffre):
+      listeDem[ligneDem].append(0)
 
-  while(ligne-1<0 and ligne-1>8):
-    print("Ligne incorrect, veuillez en choisir une autre: ", end="")
-    ligne= int(input())
+  #Affichage
+  for ligneAff in range(0, chiffre):
+    listeAff.append([])
+    for colonneAff in range(0,chiffre):
+      listeAff[ligneAff].append('☐')
+    
+  return listeDem, listeAff
+
+# Demande une ligne et une colonne dans le plateau
+# return ligne, colonne, pas de print
+def choixDeLaCase(taille):
+  print("Quelle est votre ligne : ", end="") 
+  ligne = int(input())-1
+  while(ligne<0 or ligne>=taille):
+    print("La ligne doit être comprise dans le plateau, ligne: ", end="") 
+    ligne = int(input())-1
   
-  print("Choix colonne: ", end="")
-  colonne= int(input())
-
-  while(colonne-1<0 and colonne-1>8):
-    print("Colonne incorrect, veuillez en choisir une autre: ", end="")
-    colonne= int(input())
-
+  print("Quelle est votre colonne : ", end="")
+  colonne = int(input())-1
+  while(colonne<0 or colonne>=taille):
+    print("La colonne doit être comprise dans le plateau, colonne: ", end="") 
+    colonne = int(input())-1
   return ligne, colonne
 
-#Affiche la case choisie par le joueur
-#Renvoie bool pour partie
-def AppliqueChoix(liste,matr, ligneJ, colonneJ):
-  liste[ligneJ][colonneJ] = matr[ligneJ][colonneJ]
-  AfficherMatrice(liste)
-  if(matr[ligneJ][colonneJ]=='b'):
-    return False
+# Verifie si le joueur touche une bombe et maj matrice affichage
+# pas de return, pas de print
+def verifDefaite(matriceDem, matriceAff, ligne, colonne, nbr):
+  if(matriceDem[ligne][colonne] != 'b'):
+    if(matriceDem[ligne][colonne] == 0):
+
+      matriceAff[ligne][colonne] = " "
+
+      for ligneAut in range(ligne-1, ligne+2):
+        for colonneAut in range(colonne-1,colonne+2):
+
+          if ligneAut in range(0, len(matriceDem)):
+            if colonneAut in range(0, len(matriceDem[0])):
+
+              if(matriceDem[ligneAut][colonneAut] != 'b'):
+
+                if(matriceDem[ligneAut][colonneAut] == 0):
+                  matriceAff[ligneAut][colonneAut] = " "
+
+                else:
+                  matriceAff[ligneAut][colonneAut] = matriceDem[ligneAut][colonneAut]
+
+                nbr = nbr-1
+      return True, matriceDem, matriceAff, nbr
+
+    else:
+      print("-1 si chiffre")
+      matriceAff[ligne][colonne] = matriceDem[ligne][colonne]
+      return True, matriceDem, matriceAff, nbr-1
   else:
-    return True
-# -------------------------------- Variables et autres --------------------------------
-matrice = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+    matriceAff[ligne][colonne] = matriceDem[ligne][colonne]
+    return False, matriceDem, matriceAff, nbr
 
-afficher = [['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□'],['□','□','□','□','□','□','□','□','□']]
 
-ligneJoueur = -1
-colonneJoueur = -1
-partie = True
-# -------------------------------- Initialisation --------------------------------
-matrice = AleaBombe(matrice)
-AfficherMatrice(afficher)
-AfficherMatrice(matrice)
-print("\nJeu du démineur:")
-while(partie):
-  #ligne colonne a jouer
-  ligneJoueur, colonnejoueur = ChoixJoueur()
-  partie = AppliqueChoix(afficher, matrice, ligneJoueur-1, colonnejoueur-1)
+  
+# ------------------- MATRICE  ------------------- 
+demineur = []
+afficherDem = []
 
-print("DEFAITE")
+# ------------------- VARIABLES  ------------------- 
+jouer = True
+choixAction = 0
+chiffresJoueur = 0
+nbrCases = 0
+nbrBombes = 0
+nbrVictoire = 0
+ligneJoueur = 0
+colonneJoueur = 0
+# ------------------- PRINCIPAL  ------------------- 
+print("Combien boulez vous de lignes et de colonnes ?", end=" ")
+
+#Demande de la taille de la matrice
+chiffresJoueur = int(input())
+while(chiffresJoueur <= 0):
+  print("Incorrect, votre chiffre ne peux que être supérieur à 0.\nVeuillez le reprendre: ")
+  chiffresJoueur = int(input())
+
+# Calcul de la variable comptant les cases révélées
+nbrCases = chiffresJoueur * chiffresJoueur
+nbrBombes = (nbrCases)//3
+nbrVictoire = nbrCases - nbrBombes
+
+# Creation de la matrice
+demineur, afficherDem = creerMatrice(chiffresJoueur)
+
+# Affiche la matrice d'affichage
+Afficher(afficherDem, chiffresJoueur) 
+
+# Place les bombes
+bombeAleatoire(demineur, chiffresJoueur) 
+#Afficher(demineur, chiffresJoueur) #Affiche la matrice des infos
+Afficher(demineur, chiffresJoueur)
+while(jouer == True and nbrVictoire != 0):
+  # Demande au joueur ce qu'il veut faire
+  print("Que souhaitez vous faire ?\n - Reveler une case : 1\n - Poser un drapeau : 2\n")
+  choixAction = int(input())
+  while(choixAction != 1 and choixAction !=2):
+    print("Veuillez choisir une action :\n - Reveler une case : 1\n - Poser un drapeau : 2\n")
+    choixAction = int(input())
+
+  ligneJoueur, colonneJoueur = choixDeLaCase(chiffresJoueur) # Récupére la ligne du joueur
+  if(choixAction == 1):
+    jouer, demineur, afficherDem, nbrVictoire = verifDefaite(demineur, afficherDem, ligneJoueur, colonneJoueur, nbrVictoire)
+    
+  else:
+    afficherDem[ligneJoueur][colonneJoueur] = '☒'
+  Afficher(afficherDem, chiffresJoueur)
+
+
+if(jouer == False):
+  print("Vous avez perdu, vous ferez mieux la prochaine fois.")
+else:
+  print("Bravo vous avez gagné !")
+
+#'☒'
+
